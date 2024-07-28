10aa690f7ae34ba77ac81b49d77de92097acbc15
eggr0ll <108096396+eggr0ll@users.noreply.github.com>
2024-07-27 19:37:07 -0500

    Initial data processing, GNN definition, and training on 25 .exe files.

--- genGraphs.py
+++ genGraphs.py
@@ -7,15 +7,18 @@ import matplotlib.pyplot as plt
 import torch
 import numpy as np
 import torch.nn.functional as F
+import os
+import random
+import dill
 from collections import defaultdict
 from torch_geometric.data import Data
 from torch.nn import Embedding
 from torch.nn import Linear
 from torch_geometric.nn import GCNConv
 from torch_geometric.nn import global_mean_pool
+from torch_geometric.loader import DataLoader
 
 
-G = nx.DiGraph()
 
 # STEP 1: TOKENIZE ASSEMBLY INSTRS
 # Breaks up assembly code into words
@@ -25,150 +28,186 @@ def tokenize_instructions(instruction):
 # initialize vocab dict that will contain words & their indices
 vocab = defaultdict(lambda: len(vocab))
 
-tokenized_instrs = []
-blocks = {}
-with open("results/Botnet_Android-GMBot-MALWARE-master_Android-GMBot-MALWARE-master_windows_gozi-isfb_appack.exe.output") as o_f:
-    for count, line in enumerate(o_f):
-        pass
-total_lines = count + 1
 
-with open("results/Botnet_Android-GMBot-MALWARE-master_Android-GMBot-MALWARE-master_windows_gozi-isfb_appack.exe.output") as output_file:
-    # create blocks & successors dicts
-    instr_in_block = []
-    successors = {}
-    for x in range(total_lines):
-        line = output_file.readline()
-        if "BLOCK" in line:
-            block_address = int(line[6:13])
-            block_successors = line[30:].replace(",", " ").replace("]", "").strip().split()
-            block_successors = [eval(successor) for successor in block_successors] # cast to int
-        elif "BLOCK" not in line and "None" not in line and "---" not in line:
-            instr_in_block.append(line[8:].strip().replace(",", ""))
-        # need to loop thru all instr b4 assigning it to key
-        elif "None" in line:
-            blocks[block_address] = instr_in_block
-            instr_in_block = []
-            successors[block_address] = block_successors
-            block_successors = []
-print(blocks)
-
-# tokenize instructions & add to vocab dict
-for instrs in blocks.values():
-    # tokenize all instructions in block
-    tok_block = [tokenize_instructions(instr) for instr in instrs]
-    tokenized_instrs.append(tok_block)
-    print("Tokenized block:\n", tok_block)
-
-    # add each token to vocab
-    for instr in tok_block:
-        for token in instr:
-            _ = vocab[token]
-
-#print("Tokenized instructions:\n", tokenized_instrs)
-#print("Vocab:\n", vocab)
-
-#nx.draw(G)
-#plt.show()
-#print("Total Lines:", total_lines)
-
-# STEP 2: PERFORM EMBEDDINGS
-vocab_size = len(vocab)     # should be 23
-embedding_dim = 10          # experiment w/ changing
-embedding_layer = Embedding(vocab_size, embedding_dim)
-
-# returns single 1D vector that represents entire instruction by averaging embedding vectors of individual tokens
-def embed_instr(instr):
-    tokens = instr.split()
-    token_indices = [vocab[token] for token in tokens]
-    embedded_tokens = embedding_layer(torch.tensor(token_indices))
-    return embedded_tokens.mean(dim=0)
+dir_list = os.listdir("./results")
+#print(dir_list)
 
 
-# STEP 3: ADD NODES
-for block_address, instructions in blocks.items():
-    encoded_instrs = [embed_instr(instr).detach().numpy() for instr in instructions]
-    G.add_node(block_address, instructions=encoded_instrs)
+all_graphs_max_length = 0;  # max number of features across all graphs
+class_labels = []           # holds all malware types
+graphs_as_data = []         # holds all 'data' objects
+digraphs = []                 # holds all 'DiGraph' objects
 
-# STEP 3.5: ADD EDGES
-#print("Successors:\n", successors)
-for block_address, successors in successors.items():
-    for successor in successors:
-        G.add_edge(block_address, successor)
+
+# for each graph output file in results directory
+for file in dir_list:
+    G = nx.DiGraph() 
     
-print("Nodes:\n", G.nodes(data=True))
-print("Edges:\n", G.edges())
+    full_file_path = "./results/" + file
+    print("Processing", full_file_path)
+    tokenized_instrs = []
+    blocks = {}
+    with open(full_file_path) as o_f:
+        for count, line in enumerate(o_f):
+            pass
+    total_lines = count + 1
 
+    with open(full_file_path) as output_file:
+        # create blocks & successors dicts
+        instr_in_block = []
+        successors = {}
+        for x in range(total_lines):
+            line = output_file.readline()
+            if "BLOCK" in line:
+                block_address = int(line[6:13])
+                block_successors = line[30:].replace(",", " ").replace("]", "").strip().split()
+                block_successors = [eval(successor) for successor in block_successors] # cast to int
+            elif "BLOCK" not in line and "None" not in line and "---" not in line:
+                instr_in_block.append(line[8:].strip().replace(",", ""))
+            # need to loop thru all instr b4 assigning it to key
+            elif "None" in line:
+                blocks[block_address] = instr_in_block
+                instr_in_block = []
+                successors[block_address] = block_successors
+                block_successors = []
+    print(blocks)
 
+    # tokenize instructions & add to vocab dict
+    for instrs in blocks.values():
+        # tokenize all instructions in block
+        tok_block = [tokenize_instructions(instr) for instr in instrs]
+        tokenized_instrs.append(tok_block)
+        #print("Tokenized block:\n", tok_block)
 
-# STEP 4: CONVERT TO INPUT FOR PYGEO
-# need x = node feature matrix w/ [num_nodes, num_node_features] as tensor
-# need edge_index = graph connectivity in COO format w/ [2, num_edges] as tensor
-all_node_features = []      # stores embedding vector for each node in graph
-max_length = 0
+        # add each token to vocab
+        for instr in tok_block:
+            for token in instr:
+                _ = vocab[token]
 
-for node in G.nodes(data=True):
-    if 'instructions' in node[1]:
-        flattened_node_instrs = [item for sublist in node[1]['instructions'] for item in sublist]
-        max_length = max(max_length, len(flattened_node_instrs))
-print("Max length:", max_length)
+    print("Tokenized instructions:\n", tokenized_instrs)
+    print("Vocab:\n", vocab)
 
-# data=True means node is tuple where node[0] is address and node[1] is dictionary of attributes
-for node in G.nodes(data=True):      
-    # iterates over each element in list of embedded vectors & flattens into 1 embedded vector list
-    if 'instructions' in node[1]:
-        flattened_node_instrs = [item for sublist in node[1]['instructions'] for item in sublist]
-        flattened_node_instrs = np.pad(flattened_node_instrs, (0, max_length - len(flattened_node_instrs)))     # pad w/ zeroes if list is too short
-        all_node_features.append(flattened_node_instrs)
-        print(f"Node {node[0]} padded length is {len(flattened_node_instrs)}")
-    else:
-        print(f"Node {node[0]} does not have 'instructions' attribute")
+    #nx.draw(G)
+    #plt.show()
+    #print("Total Lines:", total_lines)
+
+    # STEP 2: PERFORM EMBEDDINGS
+    vocab_size = len(vocab)     # should be 23
+    embedding_dim = 10          # experiment w/ changing
+    embedding_layer = Embedding(vocab_size, embedding_dim)
+
+    # returns single 1D vector that represents entire instruction by averaging embedding vectors of individual tokens
+    def embed_instr(instr):
+        tokens = instr.split()
+        token_indices = [vocab[token] for token in tokens]
+        embedded_tokens = embedding_layer(torch.tensor(token_indices))
+        return embedded_tokens.mean(dim=0)
     
+    # STEP 3: ADD NODES
+    for block_address, instructions in blocks.items():
+        encoded_instrs = [embed_instr(instr).detach().numpy() for instr in instructions]
+        G.add_node(block_address, instructions=encoded_instrs)
+    
+    # STEP 3.5: ADD EDGES
+    #print("Successors:\n", successors)
+    for block_address, successors in successors.items():
+        for successor in successors:
+            G.add_edge(block_address, successor)
 
-# convert node features to tensor of type float
-all_node_features_np = np.array(all_node_features)
-x = torch.tensor(all_node_features_np, dtype=torch.float)
-print("Node feature matrix shape:", {x.shape})
+    #print("Nodes:\n", G.nodes(data=True))
+    print("Edges:\n", G.edges())
 
-# create edge index tensor
-# converts edges into list of tuples -> converts list into tensor -> transposes tensor to have required shape [2, num_edges]
-edge_index = torch.tensor(list(G.edges)).t().contiguous()
+    # STEP 4: CONVERT TO INPUT FOR PYGEO
+    # need x = node feature matrix w/ [num_nodes, num_node_features] as tensor
+    # need edge_index = graph connectivity in COO format w/ [2, num_edges] as tensor
+    all_node_features = []      # stores embedding vector for each node in graph
+    max_length = 0
 
-# create obj describing graph
-data = Data(x=x, edge_index=edge_index)
+    for node in G.nodes(data=True):
+        if 'instructions' in node[1]:
+            flattened_node_instrs = [item for sublist in node[1]['instructions'] for item in sublist]
+            #print("Length of flattened node instrs:", len(flattened_node_instrs))
+            max_length = max(max_length, len(flattened_node_instrs))
+    print("Max length:", max_length)
+    # if num of features within this graph is the max among all graphs
+    if max_length > all_graphs_max_length:
+        all_graphs_max_length = max_length
 
-print("Node feature matrix:", x)
-print("Edge index tensor:", edge_index)
+    # data=True means node is tuple where node[0] is address and node[1] is dictionary of attributes
+    for node in G.nodes(data=True):      
+        # iterates over each element in list of embedded vectors & flattens into 1 embedded vector list
+        if 'instructions' in node[1]:
+            flattened_node_instrs = [item for sublist in node[1]['instructions'] for item in sublist]
+            #print("Flattened node instrs:", flattened_node_instrs)
+            flattened_node_instrs = np.pad(flattened_node_instrs, (0, max_length - len(flattened_node_instrs)))     # pad w/ zeroes if list is too short
+            #print("Flattened node instrs padded:", flattened_node_instrs)
+            all_node_features.append(flattened_node_instrs)
+            #print(f"Node {node[0]} padded length is {len(flattened_node_instrs)}")
+        else:
+            print(f"Node {node[0]} does not have 'instructions' attribute")
+
+    digraphs.append(G)
+    
+    #print("All node features:", all_node_features)
+
+    # convert node features to tensor of type float
+    all_node_features_np = np.array(all_node_features)
+    print("All node features padded:", all_node_features_np)
+    x = torch.tensor(all_node_features_np, dtype=torch.float)
+    print("Node feature matrix shape:", {x.shape})
+
+    # create edge index tensor
+    # converts edges into list of tuples -> converts list into tensor -> transposes tensor to have required shape [2, num_edges]
+    edge_index = torch.tensor(list(G.edges)).t().contiguous()
+
+    # create graph label
+    graph_label = []                            # will hold graph label/type of malware as int
+    underscore_idx = file.find("_")
+    malware_type = file[:underscore_idx]        # extracts malware type string from front of filename
+    if malware_type not in class_labels:
+        class_labels.append(malware_type)       # adds malware type string to all class labels 
+    print("Class labels:", class_labels)
+
+    index = class_labels.index(malware_type)
+    print("Index:", index)
+    graph_label.append(index)                   # gets index of malware type 
+    y = torch.tensor(graph_label)               # converts index of malware type into tensor
+    print("Graph label:", y)
+
+    # map OG node labels to contiguous "indices"
+    unique_labels = torch.unique(edge_index)    # gets node numbers
+    # creates dictionary mapping OG node labels to indices
+    label_to_index = {label.item(): index for index, label in enumerate(unique_labels)}    
+    new_edge_index = torch.tensor([[label_to_index[label.item()] for label in edge_pair] for edge_pair in edge_index.t()], dtype=torch.long).t().contiguous()
+    print("Edited edge index tensor:", new_edge_index)
+
+    # create obj describing graph
+    data = Data(x=x, edge_index=new_edge_index, y=y)
+    graphs_as_data.append(data)
+
+    print("Node feature matrix:", x)
+    #print("Edge index tensor:", edge_index)
+    print("Number of nodes:", G.number_of_nodes())
+    print("Number of node features:", len(x[1]))
+    #num_node_features = len(x[1])
+    print("Done processing", full_file_path)
+    print()
+
+print("\nMax feature length of all graphs:", all_graphs_max_length)
 
 
+# pad all graphs across all files to same length
+print("Begin padding all graphs:\n")
+for data, file in zip(graphs_as_data, dir_list):
+    print("Processing", file)
 
-'''
-# CREATE GNN
-class GCN(torch.nn.Module):
-    def __init__(self, hidden_channels):
-        super(GCN, self).__init__()
-        torch.manual_seed(12345)
-        self.conv1 = GCNConv(x[1].num_node_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.conv3 = GCNConv(hidden_channels, hidden_channels)
-        self.lin = Linear(hidden_channels, 1)       # 1 is sub for dataset.num_classes
+    data_x_padded = F.pad(data.x, pad=(0, all_graphs_max_length - data.x.shape[1], 0, 0))
+    data.x = data_x_padded
+    print("Node feature matrix:", data.x)
+    print("Node feature matrix shape:", data.x.shape)
+    
+    print("Done processing", file)
+    print()
+print("Done padding all graphs.")
 
-    def forward(self, x, edge_index, batch):
-        # 1. Obtain node embeddings 
-        x = self.conv1(x, edge_index)
-        x = x.relu()
-        x = self.conv2(x, edge_index)
-        x = x.relu()
-        x = self.conv3(x, edge_index)
-
-        # 2. Readout layer
-        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
-
-        # 3. Apply a final classifier
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.lin(x)
-        
-        return x
-
-model = GCN(hidden_channels=64)
-print(model)
-'''
+dill.dump_module("session.pkl")
---
+++ gnn.py
@@ -0,0 +1,88 @@
+import dill
+dill.load_module("session.pkl")
+
+print("Graphs:", graphs_as_data)
+# shuffle graphs & create train/test datasets
+graphs_shuffled = graphs_as_data
+random.seed(2)
+random.shuffle(graphs_shuffled)
+train_dataset = graphs_shuffled[:20]
+test_dataset = graphs_shuffled[20:]
+print("train dataset:", train_dataset)
+print("test dataset:", test_dataset)
+
+
+# batch multiple graphs into single graph
+train_loader = DataLoader(train_dataset, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=10, shuffle=False)
+
+for step, data in enumerate(train_loader):
+    print(f'Step {step + 1}:')
+    print('=======')
+    print(f'Number of graphs in the current batch: {data.num_graphs}')
+    print(data)
+    print()
+
+#'''
+# CREATE GNN
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(GCN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = GCNConv(all_graphs_max_length, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.lin = Linear(hidden_channels, 3)       # 3 is sub for dataset.num_classes
+
+    def forward(self, x, edge_index, batch):
+        # 1. Obtain node embeddings 
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        # 2. Readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+
+        # 3. Apply a final classifier
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+        
+        return x
+
+model = GCN(hidden_channels=64)
+print(model)
+#'''
+
+
+# TRAIN GNN
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+criterion = torch.nn.CrossEntropyLoss()
+
+def train():
+    model.train()
+
+    for data in train_loader:  # Iterate in batches over the training dataset.
+         out = model(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
+         loss = criterion(out, data.y)  # Compute the loss.
+         loss.backward()  # Derive gradients.
+         optimizer.step()  # Update parameters based on gradients.
+         optimizer.zero_grad()  # Clear gradients.
+
+def test(loader):
+     model.eval()
+
+     correct = 0
+     for data in loader:  # Iterate in batches over the training/test dataset.
+         out = model(data.x, data.edge_index, data.batch)  
+         pred = out.argmax(dim=1)  # Use the class with highest probability.
+         correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+     return correct / len(loader.dataset)  # Derive ratio of correct predictions.
+
+
+for epoch in range(1, 171):
+    train()
+    train_acc = test(train_loader)
+    test_acc = test(test_loader)
+    print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
